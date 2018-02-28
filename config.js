374383a05f219befb591a1ba99578089e1ca39ee
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-02-28 23:23:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ var api = "http://ipbc.pool.ahka.net:8117";
 
 var poolHost = "ipbc.pool.ahka.net";
 
-var blockExplorer = "http://blockexplorer.pw/ipbc";
+var blockExplorer = "https://explorer.ipbc.io/";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "https://explorer.ipbc.io/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "https://explorer.ipbc.io/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
